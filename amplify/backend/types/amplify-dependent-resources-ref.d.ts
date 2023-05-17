d1b672a8acb5cebbaec413fe32415623f67bfc61
Simon Payne <simon.payne@1spatial.com>
2023-05-17 14:10:26 +0100

    Add support for graphQL API on backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphql0f216b2d": {
       "AppClientID": "string",
